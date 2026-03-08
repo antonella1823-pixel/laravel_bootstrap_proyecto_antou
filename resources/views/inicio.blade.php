c5d8e81d2ba3b8d6fa660b2a2e525c1865f689da
antonella1823-pixel <insuastiantou@gmail.com>
2026-03-07 23:03:58 -0500

    Actividad Laravel Bootstrap

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Proyecto Laravel Bootstrap</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="container mt-5">
+
+<h1 class="text-center">Mi proyecto Laravel + Bootstrap</h1>
+
+<div class="alert alert-success text-center">
+Esta es una vista creada con Blade y Bootstrap
+</div>
+
+<button class="btn btn-primary">Botón Bootstrap</button>
+
+</div>
+
+</body>
+
+</html>
